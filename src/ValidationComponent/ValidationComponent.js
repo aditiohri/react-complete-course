36647e304ecdeeaa6dfd725e773906fb2cd8dfff
Aditi Ohri <didiohri@gmail.com>
2020-11-06 16:33:01 -0600

    validate output text from input if longer than or equal to five chars

--- src/ValidationComponent/ValidationComponent.js
+++ src/ValidationComponent/ValidationComponent.js
@@ -1,12 +1,28 @@
 import React from 'react'
 
 export default function ValidationComponent({ text }) {
+
+    let textLength = text ? text.length : null;
+
+    let validationText;
+
+    if (textLength && textLength >= 5) {
+        validationText = (
+            <p>
+                Text long enough
+            </p>
+        )
+    } else {
+        validationText = (
+            <p>
+                Text too short
+            </p>
+        )
+    }
+
     return (
         <div>
-            <p>
-                {text ? (text.length) : null}
-                
-                </p>
+            {validationText}
         </div>
     )
 }
